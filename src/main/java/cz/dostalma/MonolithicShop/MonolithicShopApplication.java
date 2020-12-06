80c28cfa309faa4aea4b71063fe3a9edce19fc2a
mdostal <dostalma@gmail.com>
2020-12-06 15:12:19 -0100

    second initial commit

---
+++ src/main/java/cz/dostalma/MonolithicShop/MonolithicShopApplication.java
@@ -0,0 +1,13 @@
+package cz.dostalma.MonolithicShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithicShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolithicShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/dostalma/MonolithicShop/MonolithicShopApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.dostalma.MonolithicShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithicShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
